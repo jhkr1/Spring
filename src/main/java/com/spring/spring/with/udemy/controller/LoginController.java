96677d359169a5514cb28ccfebeed1e92742990f
Jihun <wlgjs06061@naver.com>
2023-11-06 23:58:31 +0900

    로그 사용법

--- src/main/java/com/spring/spring/with/udemy/controller/LoginController.java
+++ src/main/java/com/spring/spring/with/udemy/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.spring.spring.with.udemy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    Logger logger = LoggerFactory.getLogger(getClass());
     @RequestMapping("/login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("RequestParam is "+name); // 추천하지 않는 방식.
+        logger.debug("RequestParam is {}", name);
+        logger.info("I want this is Info Level RequestParam is {}", name);
+        logger.warn("I want this is warn Level");
+//        System.out.println("RequestParam is "+name); // 추천하지 않는 방식.
         return "login";
     }
 }
